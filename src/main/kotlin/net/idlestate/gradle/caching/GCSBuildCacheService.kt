49acc4c38e19c77a2516fcd88f6a1a84339a9b3b
Nick Houghton <nhoughto@gmail.com>
2020-05-01 14:26:48 +1000

    Use ADC if no creds specified

--- src/main/kotlin/net/idlestate/gradle/caching/GCSBuildCacheService.kt
+++ src/main/kotlin/net/idlestate/gradle/caching/GCSBuildCacheService.kt
@@ -15,6 +15,7 @@
  */
 package net.idlestate.gradle.caching
 
+import com.google.auth.oauth2.GoogleCredentials
 import com.google.auth.oauth2.ServiceAccountCredentials
 import com.google.cloud.storage.Bucket
 import com.google.cloud.storage.StorageException
@@ -44,7 +45,10 @@ class GCSBuildCacheService(credentials: String, val bucketName: String, val refr
     init {
         try {
             val storage = StorageOptions.newBuilder()
-                .setCredentials(ServiceAccountCredentials.fromStream(FileInputStream(credentials)))
+                .setCredentials(credentials.isEmpty() ?
+                    GoogleCredentials.getApplicationDefault() :
+                    ServiceAccountCredentials.fromStream(FileInputStream(credentials))
+                )
                 .build()
                 .service
 
